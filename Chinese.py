c005679a7beaac2b385d8a21c78d504592391c74
james <james@jamesdeMacBook-Air.local>
2020-02-17 00:48:49 +0800

    chinese.py
    Signed-off-by: james <james@jamesdeMacBook-Air.local>

--- Chinese.py
+++ Chinese.py
@@ -1,4 +1,7 @@
 # -*- coding: UTF-8 -*-
+import matplotlib
+matplotlib.use('Agg')
+
 import matplotlib.pyplot as plt 
 plt.rcParams['font.sans-serif']=['WenQuanYi Zen Hei'] #用来正常显示中文标签 
 plt.rcParams['axes.unicode_minus']=False #用来正常显示负号 #有中文出现的情况，需要u'内容' 
