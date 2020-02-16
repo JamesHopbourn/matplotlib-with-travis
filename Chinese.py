5a20a28b9adfe0430356761080cc6e51de523b95
james <james@jamesdeMacBook-Air.local>
2020-02-17 00:56:07 +0800

    chinese.py
    Signed-off-by: james <james@jamesdeMacBook-Air.local>

--- Chinese.py
+++ Chinese.py
@@ -3,7 +3,7 @@ import matplotlib
 matplotlib.use('Agg')
 
 import matplotlib.pyplot as plt 
-plt.rcParams['font.sans-serif']=['WenQuanYi Zen Hei'] #用来正常显示中文标签 
+plt.rcParams['font.sans-serif']=['WenQuanYi Zen Hei Mono'] #用来正常显示中文标签 
 plt.rcParams['axes.unicode_minus']=False #用来正常显示负号 #有中文出现的情况，需要u'内容' 
 
 plt.clf()  # 清空画布
