5a3c2e8f1c66f9963746ee576d56db92d6fc81b9
james <james@jamesdeMacBook-Air.local>
2020-02-17 00:45:22 +0800

    chinese.py
    Signed-off-by: james <james@jamesdeMacBook-Air.local>

---
+++ Chinese.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt 
+plt.rcParams['font.sans-serif']=['WenQuanYi Zen Hei'] #用来正常显示中文标签 
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号 #有中文出现的情况，需要u'内容' 
+
+plt.clf()  # 清空画布
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.xlabel(u'横轴')
+plt.ylabel(u'纵轴')
+plt.title(u'中文标题')
+plt.show()
