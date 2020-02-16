d06f8de329b64dbe6612b268272d6bc1392a4bad
James Hopbourn <hopbourncontact@gmail.com>
2020-02-17 02:06:25 +0800

    Chinese.py

--- Chinese.py
+++ Chinese.py
@@ -7,7 +7,7 @@ plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
 plt.rcParams['axes.unicode_minus']=False #用来正常显示负号 #有中文出现的情况，需要u'内容' 
 
 plt.clf()  # 清空画布
-plt.plot([1, 2, 3], [8, 5, 7])
+plt.plot([1, 2, 3, 9], [8, 5, 7, 4])
 plt.xlabel(u'横轴 Row')
 plt.ylabel(u'纵轴 Column')
 plt.title(u'中文标题')
