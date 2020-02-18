d0ba54f207a3a7b4343a5e40907be02d49de3103
James Hopbourn <hopbourncontact@gmail.com>
2020-02-18 13:01:56 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-for file in $(find . -name "*.dot"|xargs basename)
+for file in $(find . -name "*.dot")
 do {
     filename=`echo $file|awk -F . '{print $1}'`
     dot -Tpng $file -o $filename.png
 }
 done
 
-for file in $(find . -name "*.py"|xargs basename)
+for file in $(find . -name "*.py")
 do {
     python $file
 }
